555edfa487175d816c7f9cb2166462e44fc3e294
CoderTran <tranquochieugl2@gmail.com>
2024-08-07 09:57:20 +0900

    update my source code

---
+++ Label.js
@@ -0,0 +1,31 @@
+
+        function changeLanguage(lang) {
+            var vietnameseElements = document.querySelectorAll('.vietnamese');
+            var japaneseElements = document.querySelectorAll('.japanese');
+
+            if (lang === 'vi') {
+                showElements(vietnameseElements);
+                hideElements(japaneseElements);
+            } else if (lang === 'ja') {
+                showElements(japaneseElements);
+                hideElements(vietnameseElements);
+            }
+        }
+
+        function showElements(elements) {
+            elements.forEach(function(element) {
+                element.style.display = 'block';
+            });
+        }
+
+        function hideElements(elements) {
+            elements.forEach(function(element) {
+                element.style.display = 'none';
+            });
+        }
+
+        // Set initial language to Vietnamese
+        window.onload = function() {
+            changeLanguage('vi');
+        }
+ 
