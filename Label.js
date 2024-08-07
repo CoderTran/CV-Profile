82325a0c2b3b3a52c3b15106868caa1852a507a4
CoderTran <tranquochieugl2@gmail.com>
2024-08-07 10:52:07 +0900

    update my source code

--- Label.js
+++ Label.js
@@ -1,34 +1,32 @@
+function changeLanguage(lang) {
+    var vietnameseElements = document.querySelectorAll('.vietnamese');
+    var japaneseElements = document.querySelectorAll('.japanese');
 
-        function changeLanguage(lang) {
-            var vietnameseElements = document.querySelectorAll('.vietnamese');
-            var japaneseElements = document.querySelectorAll('.japanese');
+    if (lang === 'vi') {
+        showElements(vietnameseElements);
+        hideElements(japaneseElements);
+    } else if (lang === 'ja') {
+        showElements(japaneseElements);
+        hideElements(vietnameseElements);
+    }
+}
 
-            if (lang === 'vi') {
-                showElements(vietnameseElements);
-                hideElements(japaneseElements);
-            } else if (lang === 'ja') {
-                showElements(japanesseElements);
-                hideElements(vietnameseElements);
-            }
-        }
+function showElements(elements) {
+    elements.forEach(function(element) {
+        element.style.display = 'block';
+    });
+}
 
-        function showElements(elements) {
-            elements.forEach(function(element) {
-                element.style.display = 'block';
-            });
-        }
+function hideElements(elements) {
+    elements.forEach(function(element) {
+        element.style.display = 'none';
+    });
+}
 
-        function hideElements(elements) {
-            elements.forEach(function(element) {
-                element.style.display = 'none';
-            });
-        }
-
-        // Set initial language to Vietnamese
-        window.onload = function() {
-            changeLanguage('vi');
-        }
-    
+// Set initial language to Vietnamese
+window.onload = function() {
+    changeLanguage('vi');
+}
         
    
  
